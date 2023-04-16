df4d27065f1015614c27f478f0d7bfddeee38f22
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2023-04-16 16:53:35 +0800

    1

--- script/index.ts
+++ script/index.ts
@@ -3,11 +3,11 @@ import { fetchTreeviewFromURL } from "./treeview-parser"
 import { CUSTOM_WORLD_GEN_URL } from "./fandom"
 
 !(async () => {
-    const readme=(await readFile('../readme.md')).toString();
+    const readme = (await readFile('../readme.md')).toString();
     const slots = readme.matchAll(/\| (.*?) \| \[(.*?)\].*?\|.*?\((.*?)\)\|(.*?)\|/g);
-    for(const slot of slots){
-        await writeFile(slot[2],
-        JSON.stringify(
+    for (const slot of slots) {
+        await writeFile("../" + slot[2],
+            JSON.stringify(
                 await fetchTreeviewFromURL(
                     slot[3], slot[4]
                 )
