8fce4b920f29f1123db0eff55d141936e72b4463
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2023-04-16 16:50:29 +0800

    Update index.ts

--- script/index.ts
+++ script/index.ts
@@ -4,7 +4,7 @@ import { CUSTOM_WORLD_GEN_URL } from "./fandom"
 
 !(async () => {
     const readme=(await readFile('../readme.md')).toString();
-    const slots = readme.matchAll(/\| (.*?) \| \[(.*?)\].*?\|.*?\((.*?)\)/g)\|(.*?)\|];
+    const slots = readme.matchAll(/\| (.*?) \| \[(.*?)\].*?\|.*?\((.*?)\)\|(.*?)\|/g);
     for(const slot of slots){
         await writeFile(slot[2],
         JSON.stringify(
